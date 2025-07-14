74906351941889e71735931a7accda24e0d1a684
RonitThakur2003 <thakurronit2003@gmail.com>
2025-07-14 19:45:35 +0530

    React Router

---
+++ ReactRouter/src/main.jsx
@@ -0,0 +1,30 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from './App'
+import Home from './components/Home/Home'
+import About from './components/About/About'
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <App />,
+    children: [
+      {
+        path:"",
+        element: <Home />
+      },
+      {
+        path:"about",
+        element: <About />
+      },
+    ]
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}/>
+  </StrictMode>,
+)
